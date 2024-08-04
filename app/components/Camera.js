d5eb328703525f77fd958838ad6f5a84615bd54f
gazishahi <gazishahi@gmail.com>
2024-08-04 10:16:11 -0400

    fix: partial photo UI

--- app/components/Camera.js
+++ app/components/Camera.js
@@ -1,9 +1,13 @@
+"use client";
 import React, { useState, useRef } from "react";
 import { Camera } from "react-camera-pro";
 import { Box, Button } from "@mui/material";
 import UploadIcon from "@mui/icons-material/Upload";
 import Link from "next/link";
 import { getStorage, ref, uploadBytes, uploadString } from "firebase/storage";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import CloseIcon from "@mui/icons-material/Close";
 
 function base64ToBlob(base64Data, contentType) {
   const byteCharacters = atob(base64Data);
@@ -45,15 +49,34 @@ async function uploadBase64Image(base64DataUrl, storagePath) {
 const Component = () => {
   const camera = useRef(null);
   const [image, setImage] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [showPic, setShowPic] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const uploadImage = async (event) => {
     uploadBase64Image(image, "images/photo.jpeg").catch((error) => {
       console.error("Upload failed", error);
     });
+    handleClick();
   };
 
   const showPhoto = () => {
-    console.log(image);
+    setShowPic(true);
+  };
+
+  const closePhoto = () => {
+    setShowPic(false);
   };
 
   return (
@@ -64,13 +87,23 @@ const Component = () => {
       flexDirection={"column"}
       justifyContent="center"
     >
-      <Button
-        onClick={() => setImage(camera.current.takePhoto())}
-        variant="contained"
-      >
-        Take photo
-      </Button>
-      <Camera ref={camera} aspectRatio={4 / 3} />
+      {!showPic && (
+        <Button
+          onClick={() => [setImage(camera.current.takePhoto()), showPhoto()]}
+          variant="contained"
+        >
+          Take photo
+        </Button>
+      )}
+      {!showPic && <Camera ref={camera} aspectRatio={4 / 3} />}
+      {showPic && (
+        <Box>
+          <Button startIcon={<CloseIcon />} onClick={() => closePhoto()}>
+            Close
+          </Button>
+          <img src={image} alt="Taken photo" width="470" />
+        </Box>
+      )}
       <Button
         onClick={() => uploadImage()}
         variant="contained"
@@ -78,6 +111,17 @@ const Component = () => {
       >
         Upload
       </Button>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        key={"bottom center"}
+      >
+        <Alert onClose={handleClose} severity="success">
+          Upload success!
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
